cdf83db76a3abe8e4b7422c2fe8f9d1f0a245e3b
Meek Karan <meekkaran@gmail.com>
2021-02-25 10:07:38 +0300

    first comit

---
+++ intro.py
@@ -0,0 +1,31 @@
+if 3>2:
+    print("it works!")
+else:
+    print("noo")   
+
+def hi():
+   print("hi there")
+   print("how are you")
+hi()   
+
+def hi(name):
+    if name=="meek":
+        print("hi meek")
+    elif name=="karan":
+        print ("hi karan")
+    else:
+        print("hey bitch")
+hi("karan")        
+
+
+def hi(name):
+    print("hi" + name + "!")
+girls = ["me" ,"you","us","them","hao"]
+for name in girls:
+    hi(name)
+    print("next girl")   
+
+
+for i in range(1,6):
+    print(i)
+
